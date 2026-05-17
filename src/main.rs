c06adb9cda3e0641cff849d1bb5cde6371a90a58
awoldt <abreckwoldt23@gmail.com>
2026-05-17 07:24:19 -0400

    good progess

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, error::Error, os::raw};
 
 use clap::Parser;
 use native_tls::TlsConnector;
-use postgres::{Client, Error, NoTls};
+use postgres::{Client, Error as PostgresError, NoTls};
 use postgres_native_tls::MakeTlsConnector;
+
+enum ColumnDataType {
+    SmallInt,
+    Integer,
+    BigInteger,
+    Decimal,
+    Text,
+    CharacterVarying,
+    Character,
+    Boolean,
+    Date,
+    Time,
+    TimeWithTZ,
+    Timestamp,
+    Interval,
+    Json,
+    JsonB,
+    UUID,
+    Bytea,
+    Inet,
+    Cidr,
+    Macaddr,
+    TsVector,
+    TsQuery,
+    Point,
+    Line,
+    Polygon,
+    Circle,
+    Array(Box<ColumnDataType>), // this can represent all array types
+}
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -56,6 +86,12 @@ struct DbConfig {
     require_tls: bool,
 }
 
+struct Column {
+    name: String,
+    data_type: ColumnDataType,
+    is_nullable: bool
+}
+
 fn main() {
     let args: UserInput = UserInput::parse();
 
@@ -78,7 +114,7 @@ fn main() {
 
     let tls = MakeTlsConnector::new(tls_connector);
     let source_conn_string: String = build_postgres_conn_string(source_db_config);
-    let source_client_result: Result<Client, Error>;
+    let source_client_result: Result<Client, PostgresError>;
     if args.source_requires_tls {
         source_client_result = Client::connect(&source_conn_string, tls);
     } else {
@@ -93,19 +129,13 @@ fn main() {
         }
     };
 
-
-    let tables_result = get_table_structure(&mut source_client);
-    let tables =  match tables_result {
+    let tables_result = match get_tables_structure(&mut source_client, "public") {
         Ok(x) => x,
-           Err(e) => {
+        Err(e) => {
             println!("ERROR: {:#?}", e);
             return;
         }
     };
-
-    println!("{:?}", tables);
-
-
 }
 
 fn build_postgres_conn_string(config: DbConfig) -> String {
@@ -122,35 +152,141 @@ fn build_postgres_conn_string(config: DbConfig) -> String {
     }
 }
 
-fn get_table_structure(client: &mut Client) -> Result<HashMap<String, Vec<String>>, Error> {
+fn get_tables_structure(
+    client: &mut Client,
+    schema: &str,
+) -> Result<HashMap<String, Vec<Column>>, Box<dyn Error>> {
     let results = client.query(
         "SELECT
-		t.table_name,
-		c.column_name
-		FROM information_schema.tables t
-		LEFT JOIN information_schema.columns c
-		ON c.table_schema = t.table_schema
-		AND c.table_name   = t.table_name
-		WHERE t.table_schema =  $1
-		ORDER BY t.table_name, c.ordinal_position;",
-        &[&"public"],
+            c.table_name,
+            c.column_name,
+            c.is_nullable,
+            c.udt_name,
+            c.character_maximum_length,
+            c.column_default
+        FROM information_schema.columns c
+        WHERE c.table_schema = $1
+        ORDER BY c.table_name, c.ordinal_position;",
+        &[&schema],
     )?;
 
-    let mut answer: HashMap<String, Vec<String>> = HashMap::<String, Vec<String>>::new(); // Vec<table, []columns>
+    let mut tables: HashMap<String, Vec<Column>> = HashMap::<String, Vec<Column>>::new(); // Vec<table, all columns>
 
     for row in results {
-        let table_name: &str = row.get(0);
-        let col_name: &str = row.get(1);
-        if answer.contains_key(table_name) {
-            // append this column to the table key
-            if let Some(table_cols) = answer.get_mut(table_name) {
-                table_cols.push(String::from(col_name))
-            } 
-        } else {
-            // add this table to the hash and set the first column in the vector
-            answer.insert(String::from(table_name), vec![String::from(col_name)]);
-        }
+        let table_name: &str = row.get("table_name");
+        let col_name: &str = row.get("column_name");
+        let is_nullable: &str = row.get("is_nullable");
+        let udt_name: &str = row.get("udt_name"); // udt stands for "user defined type"... the internal name postgres uses for a column type
+        let character_max_length: Option<i32> = row.get("character_maximum_length");
+        let column_default: Option<&str> = row.get("column_default");
+
+        let column_data_type = return_column_data_type(udt_name)?;
+
+        // now construct the hashmap of tables
+        tables
+            .entry(String::from(table_name))
+            .or_insert(vec![])
+            .push(Column {
+                name: String::from(col_name),
+                data_type: column_data_type,
+                is_nullable: if is_nullable == "YES" {true} else {false}
+            });
     }
 
-    Ok(answer)
+    Ok(tables)
+}
+
+fn return_column_data_type(raw_type: &str) -> Result<ColumnDataType, String> {
+    // detect an array type
+    if raw_type.starts_with("_") {
+        return match raw_type {
+            "_int2" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::SmallInt))),
+            "_int4" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Integer))),
+            "_int8" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::BigInteger))),
+    
+            "_numeric" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Decimal))),
+    
+            "_text" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Text))),
+    
+            "_varchar" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::CharacterVarying))),
+            "_bpchar" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Character))),
+    
+            "_bool" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Boolean))),
+    
+            "_date" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Date))),
+    
+            "_time" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Time))),
+            "_timetz" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::TimeWithTZ))),
+    
+            "_timestamp" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Timestamp))),
+    
+            "_interval" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Interval))),
+    
+            "_json" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Json))),
+            "_jsonb" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::JsonB))),
+    
+            "_uuid" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::UUID))),
+    
+            "_bytea" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Bytea))),
+    
+            "_inet" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Inet))),
+            "_cidr" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Cidr))),
+            "_macaddr" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Macaddr))),
+    
+            "_tsvector" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::TsVector))),
+            "_tsquery" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::TsQuery))),
+    
+            "_point" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Point))),
+            "_line" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Line))),
+            "_polygon" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Polygon))),
+            "_circle" => Ok(ColumnDataType::Array(Box::new(ColumnDataType::Circle))),
+    
+            _ => Err(format!("unknown postgres array type: {}", raw_type)),
+        };
+    } 
+
+    match raw_type {
+        "int2" => Ok(ColumnDataType::SmallInt),
+        "int4" => Ok(ColumnDataType::Integer),
+        "int8" => Ok(ColumnDataType::BigInteger),
+
+        "numeric" => Ok(ColumnDataType::Decimal),
+
+        "text" => Ok(ColumnDataType::Text),
+
+        "varchar" => Ok(ColumnDataType::CharacterVarying),
+        "bpchar" => Ok(ColumnDataType::Character),
+
+        "bool" => Ok(ColumnDataType::Boolean),
+
+        "date" => Ok(ColumnDataType::Date),
+
+        "time" => Ok(ColumnDataType::Time),
+        "timetz" => Ok(ColumnDataType::TimeWithTZ),
+
+        "timestamp" => Ok(ColumnDataType::Timestamp),
+
+        "interval" => Ok(ColumnDataType::Interval),
+
+        "json" => Ok(ColumnDataType::Json),
+        "jsonb" => Ok(ColumnDataType::JsonB),
+
+        "uuid" => Ok(ColumnDataType::UUID),
+
+        "bytea" => Ok(ColumnDataType::Bytea),
+
+        "inet" => Ok(ColumnDataType::Inet),
+        "cidr" => Ok(ColumnDataType::Cidr),
+        "macaddr" => Ok(ColumnDataType::Macaddr),
+
+        "tsvector" => Ok(ColumnDataType::TsVector),
+        "tsquery" => Ok(ColumnDataType::TsQuery),
+
+        "point" => Ok(ColumnDataType::Point),
+        "line" => Ok(ColumnDataType::Line),
+        "polygon" => Ok(ColumnDataType::Polygon),
+        "circle" => Ok(ColumnDataType::Circle),
+
+        _ => Err(format!("invalid column type {}", raw_type))
+    }
 }
